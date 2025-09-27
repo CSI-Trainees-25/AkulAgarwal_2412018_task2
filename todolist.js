317bba41f414fa9fc35d1fb0b44ececa3e5dc58e
Akul-22 <akulagrawal8758@gmail.com>
2025-09-27 11:02:08 +0530

    task-2

--- todolist.js
+++ todolist.js
@@ -1,5 +1,16 @@
 const newbox = document.getElementById("newtask");
+const newtask = document.getElementById("addtask");
+const prior = document.getElementById("priority");
+const prog = document.getElementById("progress");
+const date = document.getElementById("duedate");
 document.getElementById("addbtn").addEventListener("click", function(){
     newbox.style.display="block";
 })
 const create = document.getElementById("create");
+create.addEventListener("click", function(){
+    newbox.style.display="none";
+    const tasktitle = newtask.value.trim();
+    const priority = prior.value;
+    const progress = prog.value;
+    const duedate = date.value;
+})
