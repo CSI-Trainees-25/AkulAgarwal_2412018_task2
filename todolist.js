cb31599107fdd5298d688fdc3ee54fc8c919e346
Akul-22 <akulagrawal8758@gmail.com>
2025-09-27 16:25:34 +0530

    task-2

--- todolist.js
+++ todolist.js
@@ -10,20 +10,20 @@ document.getElementById("addbtn").addEventListener("click", function(){
 const create = document.getElementById("create");
 create.addEventListener("click", function(){
     newbox.style.display="none";
-    const tasktitle = newtask.value.trim();
-    const priority = prior.value;
-    const progress = prog.value;
-    const duedate = date.value;
+    let tasktitle = newtask.value.trim();
+    let priority = prior.value;
+    let progress = prog.value;
+    let duedate = date.value;
     left.innerHTML=`
     <div class="container">
         <h2 class="title">${tasktitle}</h2>
         <hr>
-        <select id="progr">
+        <select id="progress">
             <option>Done</option>
             <option>In Progress</option>
             <option>Not Started</option>
         </select>
-        <select id="priori">
+        <select id="priority">
             <option>Low</option>
             <option>Medium</option>
             <option>High</option>
